31acf2473d880d83f893e683a30ff544fd75b5d7
Nawaraj <praabiin.rai@gmail.com>
2022-03-01 23:32:11 +0545

    Used Model Serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,18 +7,20 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
     '''
     DRF API View
     '''
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
     '''Serialization:
         model instance (model_data)
         turn it into Python dict
         return JSON to my client'''
-    if model_data:
-        data = model_to_dict(data, fields=['id', 'title', 'price'])
+    if instance:
+        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
