8703df414bfe4472636260ec8e5982f3f06c3d7d
Nawaraj <praabiin.rai@gmail.com>
2022-03-01 16:41:04 +0545

    Created my first API View

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,9 @@
 import requests
 
-endpoint = "https://www.github.com"
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint) # HTTP Request
+print(get_response.text)
+print(get_response.status_code)
+
+print(get_response.json())
