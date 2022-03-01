eb54968c91072458ecf558961887dcb9a9388b41
Nawaraj <praabiin.rai@gmail.com>
2022-03-02 00:50:37 +0545

    Ingested Data with DRF Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,18 +9,24 @@ from rest_framework.decorators import api_view
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(["GET", "POST"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     '''
     DRF API View
     '''
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    '''Serialization:
-        model instance (model_data)
-        turn it into Python dict
-        return JSON to my client'''
-    if instance:
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+
+    # instance = Product.objects.all().order_by("?").first()
+    # data = {}
+    # '''Serialization:
+    #     model instance (model_data)
+    #     turn it into Python dict
+    #     return JSON to my client'''
+    # if instance:
+    #     # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+    #     data = ProductSerializer(instance).data
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,8 +2,8 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"product_id": 123}) # HTTP Request
-print(get_response.text)
+get_response = requests.post(endpoint, json={"title": "ABC", "content": "Hello World", "price": "ABC"}) # HTTP Request
+# print(get_response.text)
 # print(get_response.headers)
 
 print(get_response.json())
